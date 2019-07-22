1a9b7fdbfa7a5eaad865a6c9140e48c49087a22b
Zach Smith <kerbonauts@gmail.com>
2019-07-22 16:20:45 -0700

    Update InstallTestNavAZMerit.bat
    Now elevates the script.

--- InstallTestNavAZMerit.bat
+++ InstallTestNavAZMerit.bat
@@ -1,7 +1,52 @@
+::::::::::::::::::::::::::::::::::::::::::::
+:: Automatically check & get admin rights V2
+::::::::::::::::::::::::::::::::::::::::::::
+@echo off
+CLS
+ECHO.
+ECHO =============================
+ECHO Running Admin shell
+ECHO =============================
+
+:init
+setlocal DisableDelayedExpansion
+set "batchPath=%~0"
+for %%k in (%0) do set batchName=%%~nk
+set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
+setlocal EnableDelayedExpansion
+
+:checkPrivileges
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
+ECHO.
+ECHO **************************************
+ECHO Invoking UAC for Privilege Escalation
+ECHO **************************************
+
+ECHO Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
+ECHO args = "ELEV " >> "%vbsGetPrivileges%"
+ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
+ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
+ECHO Next >> "%vbsGetPrivileges%"
+ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+"%SystemRoot%\System32\WScript.exe" "%vbsGetPrivileges%" %*
+exit /B
+
+:gotPrivileges
+setlocal & pushd .
+cd /d %~dp0
+if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
+
+::::::::::::::::::::::::::::
+::START
+::::::::::::::::::::::::::::
 rem Place the TestNav and AZMerit msi installers in the same directory as the script.
 rem You may need to change the file names of the installers as they are updated.
 rem "%~dp0" refers to the directory where the script is currently executing from.
 
 cd "%~dp0"
-msiexec /passive /package "%~dp0testnav-1.8.2.msi"
-msiexec /passive /package "%~dp0azmerit-10.4.msi"
+msiexec /i "%~dp0testnav-1.8.2.msi" /passive /norestart
+msiexec /i"%~dp0azmerit-10.4.msi" /passive /norestart
