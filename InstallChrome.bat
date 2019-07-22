32da29576d358a4d922826aea86410cbf0216c09
Zach Smith <kerbonauts@gmail.com>
2019-07-22 16:18:07 -0700

    Update InstallChrome.bat

--- InstallChrome.bat
+++ InstallChrome.bat
@@ -1,9 +1,52 @@
-@echo off
-
 rem NOTE: use "rem" to add comments.
 
-echo Installing the latest version of Chrome.
+::::::::::::::::::::::::::::::::::::::::::::
+:: Automatically check & get admin rights V2
+::::::::::::::::::::::::::::::::::::::::::::
+@echo off
+CLS
+ECHO.
+ECHO =============================
+ECHO Running Admin shell
+ECHO =============================
 
+:init
+setlocal DisableDelayedExpansion
+set "batchPath=%~0"
+for %%k in (%0) do set batchName=%%~nk
+set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
+setlocal EnableDelayedExpansion
+
+:checkPrivileges
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
+ECHO.
+ECHO **************************************
+ECHO Invoking UAC for Privilege Escalation
+ECHO **************************************
+
+ECHO Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
+ECHO args = "ELEV " >> "%vbsGetPrivileges%"
+ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
+ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
+ECHO Next >> "%vbsGetPrivileges%"
+ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+"%SystemRoot%\System32\WScript.exe" "%vbsGetPrivileges%" %*
+exit /B
+
+:gotPrivileges
+setlocal & pushd .
+cd /d %~dp0
+if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
+
+::::::::::::::::::::::::::::
+::START
+::::::::::::::::::::::::::::
+
+echo Installing the latest version of Chrome.
 rem Tests for whether the system is 32-bit or 64-bit.
 echo Testing for bitness.
 reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
@@ -24,8 +67,8 @@ echo Done!
 
 rem Run the Chrome installer.
 echo Installing Chrome...
-if %OS%==32BIT msiexec /norestart /i /qn "%~dp0googlechrome.msi"
-if %OS%==64BIT msiexec /norestart /i /qn "%~dp0googlechrome64.msi"
+if %OS%==32BIT msiexec /i "%~dp0googlechrome.msi" /passive /norestart
+if %OS%==64BIT msiexec /i "%~dp0googlechrome64.msi" /passive /norestart
 echo Done!
 
 echo Installing preferences.
